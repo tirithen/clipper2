d8eeeeb207ff9dd19545958dcc67d5648b2769a7
Fredrik Söderström <tirithen@gmail.com>
2024-03-31 10:35:11 +0200

    style: fix code formatting

--- examples/union.rs
+++ examples/union.rs
@@ -1,18 +1,8 @@
 use clipper2::{union, FillRule, Paths};
 
 fn main() {
-    let path1 = vec![
-        [1.0, 0.0],
-        [10.0, 0.0],
-        [10.0, 10.0],
-        [0.0, 10.0],
-    ];
-    let path2 = vec![
-        [5.0, 5.0],
-        [15.0, 5.0],
-        [15.0, 15.0],
-        [5.0, 15.0],
-    ];
+    let path1 = vec![[1.0, 0.0], [10.0, 0.0], [10.0, 10.0], [0.0, 10.0]];
+    let path2 = vec![[5.0, 5.0], [15.0, 5.0], [15.0, 15.0], [5.0, 15.0]];
 
     let output = union(&Paths::from(vec![path1, path2]), FillRule::NonZero);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-mod clipper2;
 mod boolean;
+mod clipper2;
 
 #[cfg(test)]
 mod tests;
 
-pub use clipper2::{Paths,Point};
 pub use boolean::{union, FillRule};
+pub use clipper2::{Paths, Point};
