110465dcbbca6611b56dc2665bb11263c365df2b
Fredrik Söderström <tirithen@gmail.com>
2024-03-31 10:43:23 +0200

    test: add a third shape to union test

--- src/tests.rs
+++ src/tests.rs
@@ -16,8 +16,8 @@ fn test_paths_from_into() {
 fn test_union() {
     let shape1 = vec![(1.0, 1.0), (1.0, 5.0), (5.0, 5.0), (5.0, 1.0)];
     let shape2 = vec![(2.0, 2.0), (2.0, 4.0), (8.0, 4.0), (8.0, 2.0)];
-    // let shape3 = vec![(20.0, 20.0), (20.0, 40.0), (80.0, 40.0), (80.0, 20.0)];
-    let shapes = vec![shape1, shape2 /*shape3*/];
+    let shape3 = vec![(20.0, 20.0), (20.0, 40.0), (80.0, 40.0), (80.0, 20.0)];
+    let shapes = vec![shape1, shape2 shape3];
 
     let paths = Paths::from(shapes);
     let result = union(&paths, FillRule::NonZero);
@@ -25,6 +25,6 @@ fn test_union() {
     assert_eq!(result.iter().count(), 2);
     assert_eq!(
         result.to_vec(),
-        Paths::from(vec![vec![(1.0, 1.0), (1.0, 5.0), (5.0, 5.0), (5.0, 1.0)]]).to_vec()
+        Paths::from(vec![vec![(1.0, 1.0) /* fix expected output once there are no more crashes */]]).to_vec()
     );
 }
